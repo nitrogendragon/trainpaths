ccef93320a391ae0cc44b347b7a0c19c0fd7047c
User <User@10.200.3.11>
2016-03-18 13:44:55 -0500

    cool body project

--- src/nbody/Body.java
+++ src/nbody/Body.java
@@ -1,34 +1,77 @@
 package nbody;
 
 import edu.princeton.cs.StdDraw;
+import java.awt.Color;
 
-/******************************************************************************
- *  Compilation:  javac Body.java
- *  Execution:    java Body
- *  Dependencies: Vector.java StdDraw.java
- *
- *  Implementation of a 2D Body with a position, velocity and mass.
- *
- *
- ******************************************************************************/
 
+/**
+ * ****************************************************************************
+ * Compilation: javac Body.java Execution: java Body Dependencies: Vector.java
+ * StdDraw.java
+ *
+ * Implementation of a 2D Body with a position, velocity and mass.
+ *
+ *
+ *****************************************************************************
+ */
 public class Body {
+
+    private final double diameter; // planet size
     private Vector r;      // position
     private Vector v;      // velocity
     private final double mass;   // mass
+    private final Color color;  // color
+    private final double universeradius; //radius of universe
 
-    public Body(Vector r, Vector v, double mass) {
+    /**
+     *
+     * @param r//position of body initial
+     * @param v//velocity of body
+     * @param diameter//diameter of body
+     * @param mass//mass of body
+     * @param color//color of body
+     * @param universeradius// radius of universe
+     */
+    public Body(Vector r, Vector v, double diameter, double mass, Color color, double universeradius) {//added color to body
         this.r = r;
         this.v = v;
+        this.diameter = diameter;
         this.mass = mass;
-    } // Body( Vector, Vector, double )
+        this.color = color;
+        this.universeradius = universeradius;
+    } // Body( Vector, Vector, double, color )
 
+    /**
+     *
+     * @param f//?
+     * @param dt//?
+     */
     public void move(Vector f, double dt) {
-        Vector a = f.times(1/mass);
-        v = v.plus(a.times(dt));
-        r = r.plus(v.times(dt));
+        Vector a = f.times(1 / mass);
+        setVelocity(getVelocity().plus(a.times(dt)));
+        r = r.plus(getVelocity().times(dt));
+        
+        double xCoord = this.r.cartesian(0);
+        double yCoord = this.r.cartesian(1);
+        if (xCoord > universeradius || xCoord < -universeradius){
+            // reverse x velocity
+            double[] factors = {-1.0, 1.0};
+            this.v = this.v.times(factors);
+        }
+        if (yCoord > universeradius || yCoord < -universeradius){
+            // reverse y velocity
+            double[] factors = {1.0, -1,0};
+            this.v = this.v.times(factors);
+        }
+            
+
     } // move( Vector, double )
 
+    /**
+     *
+     * @param b//?
+     * @return//?
+     */
     public Vector forceFrom(Body b) {
         Body a = this;
         double G = 6.67e-11;
@@ -38,15 +81,27 @@ public class Body {
         return delta.direction().times(F);
     } // forceFrom( Body )
 
+    /**
+     *
+     */
     public void draw() {
-        StdDraw.setPenRadius(0.025);
+        StdDraw.setPenColor(color);//adds color to drawing
+        StdDraw.setPenRadius(this.diameter);//new size of planets
         StdDraw.point(r.cartesian(0), r.cartesian(1));
-    } // draw()
+    }
 
-    // this method is only needed if you want to change the size of the bodies
-    public void draw(double penRadius) {
-        StdDraw.setPenRadius(penRadius);
-        StdDraw.point(r.cartesian(0), r.cartesian(1));
-    } // draw( double )
-  
-} // Body
+    /**
+     * @return the v
+     */
+    public Vector getVelocity() {
+        return v;
+    }
+
+    /**
+     * @param v the v to set
+     */
+    public void setVelocity(Vector v) {
+        this.v = v;
+    }
+} 
+// draw()
